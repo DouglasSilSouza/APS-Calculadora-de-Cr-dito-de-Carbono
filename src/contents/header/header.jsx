1b2dd4618addfad5ed1d104ceec3d5bb623c3959
DouglasSilSouza <97581286+DouglasSilSouza@users.noreply.github.com>
2024-10-22 00:24:38 -0300

    modified:   src/App.css 	new file:   src/assets/images/Ecosoma-copia.png 	modified:   src/contents/header/header.jsx

--- src/contents/header/header.jsx
+++ src/contents/header/header.jsx
@@ -1,11 +1,27 @@
-
+import logo from '../../assets/images/Ecosoma-copia.png';
 
 function Header () {
     return (
         <>
-            <div className="container text-center">
+        <nav className="navbar" style={{backgroundColor: "#bcfacc"}}>
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Ecosoma" 
+                        style={{width: '15%',}}
+                    />
+                </a>
+            </div>
+        </nav>
+            <div className="container text-center"
+                style={{
+                    backgroundColor: "#7ab562",
+                    borderRadius: "10px",
+                    color: "#FFFFFF",
+                    marginTop: "1rem",
+                }}
+            >
                 <h1>NPS - Calculadora de Crédito de carbono</h1>
-                <p>Pelos Alunos: Douglas Silva, Leandro Santos, Victor, Victoria e Wendel</p>
+                <p>Pelos Alunos: DOUGLAS SILVA DE SOUZA, LEANDRO SANTOS PEREIRA , VITOR DA PAIXÃO, VITORIA GOES CRUZ e WENDEL GUSTAVO DA SILVA BRITO</p>
             </div>
         </>
     )
