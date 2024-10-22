1684affc43041ea96d990289760701e09377fcca
DouglasSilSouza <97581286+DouglasSilSouza@users.noreply.github.com>
2024-10-22 00:30:20 -0300

    modified:   src/contents/header/header.jsx

--- src/contents/header/header.jsx
+++ src/contents/header/header.jsx
@@ -12,17 +12,18 @@ function Header () {
                 </a>
             </div>
         </nav>
-            <div className="container text-center"
-                style={{
-                    backgroundColor: "#7ab562",
-                    borderRadius: "10px",
-                    color: "#FFFFFF",
-                    marginTop: "1rem",
-                }}
-            >
-                <h1>NPS - Calculadora de Crédito de carbono</h1>
-                <p>Pelos Alunos: DOUGLAS SILVA DE SOUZA, LEANDRO SANTOS PEREIRA , VITOR DA PAIXÃO, VITORIA GOES CRUZ e WENDEL GUSTAVO DA SILVA BRITO</p>
-            </div>
+        <div className="text-center"
+            style={{
+                backgroundColor: "#7ab562",
+                borderRadius: "10px",
+                color: "#FFFFFF",
+                margin: "1rem 0.5rem",
+                padding: "0.5rem 0.5rem"
+            }}
+        >
+            <h1>NPS - Calculadora de Crédito de carbono</h1>
+            <p>Pelos Alunos: DOUGLAS SILVA DE SOUZA, LEANDRO SANTOS PEREIRA , VITOR DA PAIXÃO, VITORIA GOES CRUZ e WENDEL GUSTAVO DA SILVA BRITO</p>
+        </div>
         </>
     )
 }
